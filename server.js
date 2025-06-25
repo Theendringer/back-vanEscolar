f1ebdc10eef0d8499a528ba64465bbc6e3eec5ab
Gustavo Endringer Pereira <82174986+Theendringer@users.noreply.github.com>
2025-06-24 22:36:46 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const dotenv = require('dotenv');
 dotenv.config();
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
